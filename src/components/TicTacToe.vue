f3ea89a3c769aae0cd42d00d38d72adfe81bceeb
muchuan <mu.chuan.hung@gmail.com>
2022-01-22 01:11:40 +0800

    refractor: 重構圈圈叉叉

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -3,10 +3,17 @@
   <div class="container">
     <div class="block">   
     </div>
+    
     <div class="block">
       <div class="block_circle">
       </div> 
     </div>  
+
+    <div class="block">
+      <div class="block_cross">
+      </div> 
+    </div>  
+
   </div>
 </template>
 
@@ -17,14 +24,17 @@ $color_blue: #46f;
 $color_red: #f35;
 $color_bg: #222;
 
+
+
 .block {
   width: 150px;
   height: 150px;
   border: 1px solid ;
   position: relative;
+  
 }
 
-
+ /*---區塊元素圈圈---*/
 .block_circle{
     /*添加偽元素*/
     &:after,&:before{
@@ -36,9 +46,8 @@ $color_bg: #222;
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
-     
+      transition: 0.5s;
    }
-   /*---區塊元素圈圈---*/
    &:before{
      width: 135px;
      height: 135px;
@@ -49,26 +58,35 @@ $color_bg: #222;
      height: 90px;
      background-color: $color_bg;
    }
-   /*---區塊元素叉叉---*/
-   &:hover{
-    &:after,&:before{
-     border-radius: 0px;
-     width: 135px;
-     height: 22.5px;
-    }
-    &:before{
-    
-      background-color: $color_blue;
-      transform: translate(-50%, -50%) rotate(45deg)
-   }
-    &:after{
-      background-color: $color_blue;
-      transform: translate(-50%, -50%) rotate(-45deg)
-     }
-   }
-   
 }
 
+ /*---區塊元素叉叉---*/
+.block_cross{
+  /*添加偽元素*/
+  &:after,&:before{
+    content: "";
+    display: block;  
+    border-radius: 0px;
+    /*區塊元素垂直置中*/
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    transition: 0.5s;
+  }
+    &:before{
+      width: 135px;
+      height: 22.5px;
+      background-color: $color_blue;
+      transform: translate(-50%, -50%) rotate(45deg)
+  }
+    &:after{
+      width: 135px;
+      height: 22.5px;
+      background-color: $color_blue;
+      transform: translate(-50%, -50%) rotate(-45deg)
+  }
+}
 
 
 </style>
